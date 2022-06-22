9804e26d7f30304fa94a85a1fff5e36a400b982f
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-06-22 19:17:21 +0700

    update yang tanpa inbuilt

--- task2.js
+++ task2.js
@@ -1,5 +1,25 @@
 // tanpa built-in
+function membalikKata(teks) {
+    hasil = ""
+    hasilBalik = ""
+    final1 = ""
+    for (let x = teks.length - 1; x >= 0; x--) {
+        hasil = hasil + teks[x]
+    }
+    let final = hasil.length
+    for (let y = 0; y <= final; y++) {
+        if (hasil[y] == ' ') {
+            for (let z = y - 1; z >= 0; z--) {
+                hasilBalik = hasilBalik + hasil[z]
+                y++
+            }
+            final1 = hasilBalik + " a"
+        }
+    }
+    console.log(final1)
+}
 
+membalikKata('Saya belajar Javascript')
 
 // dengan built-in
 // test
