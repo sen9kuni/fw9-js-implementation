5cb512a55095cff60f9f5a124307a1e08cdf80a1
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-06-22 15:30:32 +0700

    tugas reverse word

---
+++ task2.js
@@ -0,0 +1,13 @@
+// tanpa built-in
+
+
+// dengan built-in
+
+function reverseWord2(texts) {
+    let splitText = texts.split(" ")
+    let reverseText = splitText.reverse()
+    let jointext = reverseText.join(" ")
+    console.log(jointext)
+}
+
+reverseWord2('Saya belajar Javascript')
