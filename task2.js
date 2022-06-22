a0365d0fd44f17aa854259bc8422fcadd01d8f50
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-06-22 22:07:55 +0700

    penambahan yang tanpa inbuilt

--- task2.js
+++ task2.js
@@ -1,11 +1,22 @@
 // tanpa built-in
 function membalikKata(teks) {
-    hasil = ""
-    hasilBalik = ""
-    final1 = ""
+    let hasil = ""
+    let hasilBalik = ""
+    let hasilBalik1 = ""
+    let hasilBalik2 = ""
+    let final1 = ""
+    let final2 = ""
+    let final3 = ""
+    let finaltemp = ""
+
+
+
     for (let x = teks.length - 1; x >= 0; x--) {
         hasil = hasil + teks[x]
     }
+    console.log(hasil)
+
+
     let final = hasil.length
     for (let y = 0; y <= final; y++) {
         if (hasil[y] == ' ') {
@@ -13,10 +24,30 @@ function membalikKata(teks) {
                 hasilBalik = hasilBalik + hasil[z]
                 y++
             }
-            final1 = hasilBalik + " a"
+            final1 = hasilBalik + " "
         }
     }
-    console.log(final1)
+
+    let test = final1.length
+    console.log(test);
+    for (let y = test; y <= final; y++) {
+        if (hasil[y] == ' ') {
+            for (let z = y - 1; z >= test; z--) {
+                hasilBalik1 = hasilBalik1 + hasil[z];
+                y++
+            }
+            final2 = hasilBalik1 + " "
+        }
+    }
+
+    let test2 = test + final2.length - 1
+    for (let i = final - 1; i >= test2; i--) {
+        hasilBalik2 = hasilBalik2 + hasil[i]
+    }
+    final3 = final3 + hasilBalik2
+
+    finaltemp = `${final1}${final2}${final3}`
+    console.log(finaltemp)
 }
 
 membalikKata('Saya belajar Javascript')
