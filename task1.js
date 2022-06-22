13ddf6cc03b2e5354447e24e477a3c3f68fedcf2
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-06-22 14:46:25 +0700

    tugas deteksi palindrom

---
+++ task1.js
@@ -0,0 +1,36 @@
+// tanpa built-in
+function checkPalindrome1(teks) {
+    let hasil = ""
+    for (let x = teks.length - 1; x >= 0; x--) {
+        hasil = hasil + teks[x]
+    }
+    if (hasil === teks) {
+        console.log('palindrom')
+    } else {
+        console.log('not palindrom');
+    }
+
+}
+
+checkPalindrome1('karak')
+
+
+// dengan built-in
+function checkPalindrome2(text) {
+    // versi singkat
+    let splitText = text.split("").reverse().join("")
+
+    // ver agak panjang
+    // let splitText = text.split("")
+    // let reverseSplit = splitText.reverse()
+    // let joinReverse = reverseSplit.join("")
+
+    // jika yang agak panjang if yang splitText diganti dengan joinReverse
+    if (splitText === text) {
+        console.log('palindrom')
+    } else {
+        console.log('not palindrom');
+    }
+}
+
+checkPalindrome2('bukan')
