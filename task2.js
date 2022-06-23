5d37eb7cdd153ea59f62ca5b76bbf2307608e541
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-06-23 18:34:39 +0700

    fix: change all code

--- task2.js
+++ task2.js
@@ -1,58 +1,26 @@
 // tanpa built-in
-function membalikKata(teks) {
-    let hasil = ""
-    let hasilBalik = ""
-    let hasilBalik1 = ""
-    let hasilBalik2 = ""
-    let final1 = ""
-    let final2 = ""
-    let final3 = ""
-    let finaltemp = ""
-
-
-
-    for (let x = teks.length - 1; x >= 0; x--) {
-        hasil = hasil + teks[x]
-    }
-
-
-    let final = hasil.length
-    for (let y = 0; y <= final; y++) {
-        if (hasil[y] == ' ') {
-            for (let z = y - 1; z >= 0; z--) {
-                hasilBalik = hasilBalik + hasil[z]
-                y++
-            }
-            final1 = hasilBalik + " "
+function reverseTexts(texts) {
+    let temp = ""
+    let Array = []
+    let final = ""
+    for (let i = 0; i <= texts.length; i++) {
+        if (texts[i] === " " || i === texts.length) {
+            Array[Array.length] = temp
+            temp = ""
+            i += 1
         }
+        temp += texts[i]
     }
 
-    let test = final1.length
-    for (let y = test; y <= final; y++) {
-        if (hasil[y] == ' ') {
-            for (let z = y - 1; z >= test; z--) {
-                hasilBalik1 = hasilBalik1 + hasil[z];
-                y++
-            }
-            final2 = hasilBalik1 + " "
-        }
+    for (let z = Array.length - 1; z >= 0; z--) {
+        final += Array[z] + " ";
     }
-
-    let test2 = test + final2.length - 1
-    for (let i = final - 1; i >= test2; i--) {
-        hasilBalik2 = hasilBalik2 + hasil[i]
-    }
-    final3 = final3 + hasilBalik2
-
-    finaltemp = `${final1}${final2}${final3}`
-    console.log(finaltemp)
+    console.log(final)
 }
 
-membalikKata('Saya belajar Javascript')
+reverseTexts("Saya belajar Javascript")
 
 // dengan built-in
-// test
-// 
 
 function reverseWord2(texts) {
     let splitText = texts.split(" ")
