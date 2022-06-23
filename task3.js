cb1320205e7fa4fdc763b1f9ac86b9bcd8e7f180
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-06-23 14:05:15 +0700

    fix: revisi

--- task3.js
+++ task3.js
@@ -1,23 +1,18 @@
 // dengan built-in
 
 function divdeAndSort(number) {
-    let tempNum = ("" + number).split(0);
+    let finalNumber = ""
+    let tempNum = number.toString().split(0);
 
-    function numberToArray(number) {
-        let array = number.toString().split("")
-        return array.map(x => parseInt(x))
-    }
-    let numArray1 = numberToArray(tempNum[0])
-    let numArray2 = numberToArray(tempNum[1])
-    let numArray3 = numberToArray(tempNum[2])
-    let sortNum1 = numArray1.sort((a, b) => a - b)
-    let sortNum2 = numArray2.sort((a, b) => a - b)
-    let sortNum3 = numArray3.sort((a, b) => a - b)
-    let conNum = sortNum1.concat(sortNum2, sortNum3)
-    let joinNum = conNum.join('')
-    let final = parseInt(joinNum)
-    console.log(final)
+    tempNum.map(item => {
+        let newArrayNum = item.split('')
+
+        finalNumber += newArrayNum.sort((a, b) => {
+            return a - b
+        }).join('')
+    })
+    return parseInt(finalNumber)
 }
 
 
-divdeAndSort(881120113260321)
+console.log(divdeAndSort(881120113260321))
